90aa8bc5438065db1abce7466cbbeba7656e7bf7
Zorz42 <jakob@zorz.si>
2019-12-08 19:57:53 +0100

    fixed connection bug for macos

--- jpm-sources/scripts/checkforinternetconnection.py
+++ jpm-sources/scripts/checkforinternetconnection.py
@@ -1,7 +1,9 @@
 import urllib.request
+import ssl
 
 
 def check_internet_connection():
+    ssl._create_default_https_context = ssl._create_unverified_context
     try:
         data = urllib.request.urlopen("https://google.com")
         return True
