70f58799bb5ade0510ed24165fef46d25915be19
Zorz42 <jakob@zorz.si>
2020-03-13 12:21:58 +0100

    relocating data from /usr/local/bin to /usr/local/share

--- jpm-sources/scripts/upgrader.py
+++ jpm-sources/scripts/upgrader.py
@@ -53,7 +53,7 @@ def forceupgrade(version_to_install):
     print_normal("DONE")
     print_normal("Installing jpm...")
     system("cd " + currentdir + "jpm-" + str(
-        version_to_install) + " && cp -r jpm-sources /usr/local/bin/")
+        version_to_install) + " && cp -r jpm-sources /usr/local/share/")
     print_debug("Cleaning up ... ", end='', flush=True)
     remove(currentdir + "newerjpm.zip")
     rmtree(currentdir + "jpm-" + str(version_to_install))
