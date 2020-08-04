371015102bb5e124bbe4571a371026e20c078051
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2020-08-04 19:28:31 +0200

    added sh back

--- Sources/main.py
+++ Sources/main.py
@@ -1,10 +1,14 @@
-#!sudo python3
-
 from sys import version_info, argv
+from subprocess import call
 
 from globals import throwError
 
-version = "1.6.10"
+version = "1.6.11"
+
+
+def getSuperuserPrivileges():
+    if call(["/usr/bin/sudo", "/usr/bin/id"]) != 0:
+        exit(0)
 
 
 def main():
@@ -19,18 +23,22 @@ def main():
 
     arg = argv[1]
     args = argv[2:]
-    from scripts.upgrader import checkForJaclangUpdate
-    checkForJaclangUpdate()
 
     if arg == "install":
         from scripts.install import install
         from scripts.checkForRepositoryConnection import checkConnection
+        from scripts.upgrader import checkForJaclangUpdate
 
+        getSuperuserPrivileges()
+        checkForJaclangUpdate()
         checkConnection()
         install(set(args))
     elif arg == "remove":
         from scripts.remove import removePackages
+        from scripts.upgrader import checkForJaclangUpdate
 
+        getSuperuserPrivileges()
+        checkForJaclangUpdate()
         removePackages(set(args))
     elif arg == "list":
         from scripts.listPackages import listPackages
@@ -38,27 +46,36 @@ def main():
         listPackages()
     elif arg == "cleanup":
         from scripts.cleanup import cleanup
+        from scripts.upgrader import checkForJaclangUpdate
 
+        getSuperuserPrivileges()
+        checkForJaclangUpdate()
         cleanup()
     elif arg == "upgrade":
         from scripts.upgrader import upgrade
         from scripts.checkForRepositoryConnection import checkConnection
 
+        getSuperuserPrivileges()
         checkConnection()
         upgrade()
     elif arg == "listall":
         from scripts.listAll import listall
+        from scripts.upgrader import checkForJaclangUpdate
 
+        getSuperuserPrivileges()
+        checkForJaclangUpdate()
         listall()
     elif arg == "updatedatabase":
         from scripts.updateDatabase import updateDatabase
         from scripts.checkForRepositoryConnection import checkConnection
 
+        getSuperuserPrivileges()
         checkConnection()
         updateDatabase()
     elif arg == "uninstall":
         from scripts.uninstall import uninstall
 
+        getSuperuserPrivileges()
         uninstall()
     else:
         throwError("Unknown argument: " + arg)
